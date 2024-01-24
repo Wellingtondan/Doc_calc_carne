8e8de71099a68d4f197e8b5e1ff663651e50b4f6
Wellington Dan <119419112+Wellingtondan@users.noreply.github.com>
2024-01-24 07:47:23 -0300

    Update 5.Conferência tributações de carne aba indústri.sql

--- 5.Conferência tributações de carne aba indústri.sql
+++ 5.Conferência tributações de carne aba indústri.sql
@@ -1,6 +1,3 @@
-5.Conferência tributações de carne aba indústria
-
-
 /* A parte do script (c.PERTRIBUTADO IN (58.33, 58.34, 38.89)) é para identificar todas as tributações que estão com a redução para cesta básica,
 isso é para uma análise completa considerando todas as tributações, apenas para conferir se todas as tributações relacionadas a carne estão parametrizadas 
 com a TIPOCALCICMSFISCI (utilizar o código 21 - "21 - ICMS Calculado - GIA do MS") / para filtro apenas das tributações com código 21 c.TIPOCALCICMSFISCI = 21 */
@@ -8,7 +5,7 @@ com a TIPOCALCICMSFISCI (utilizar o código 21 - "21 - ICMS Calculado - GIA do M
 SELECT 
     MAX(a.NROTRIBUTACAO) AS NROTRIBUTACAO, -- ou MIN(a.nrotributacao)
     c.UFCLIENTEFORNEC,
-    SUBSTRING_INDEX(SUBSTRING_INDEX(TRIBUTACAO, 'NCM ', -1), ' CEST', 1) AS NCM_Code,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(TRIBUTACAO, 'NCM ', -1), ' CEST', 1) AS NCM,
     SUBSTRING_INDEX(SUBSTRING_INDEX(TRIBUTACAO, ' CEST ', -1), ' ', 1) AS CEST,
     a.TRIBUTACAO, 
     a.DESCAPLICACAO,
@@ -26,14 +23,24 @@ SELECT
     c.PERISENTOCALC, 
     c.PEROUTROCALC, 
     c.PERALIQICMSCALCPRECO, 
-    c.TIPOCALCICMSFISCI
+    c.TIPOCALCICMSFISCI,
+    c.SITUACAONF,
+    c.SITUACAONFCALC,
+    c.PERICMSRESOLUCAO13,	
+    c.PERTRIBUTADORESOL13,
+    c.PERISENTORESOL13,
+    c.PEROUTRORESOL13,
+    c.PERACRESCSTRESOLUCAO13,
+    c.PERALIQICMSCALCRESOL13   
 FROM
     db_trib_rev.tb_capas_fonseca_sp a
 INNER JOIN db_trib_rev.tb_convenio_atual b ON SUBSTRING_INDEX(SUBSTRING_INDEX(TRIBUTACAO, ' CEST ', -1), ' ', 1) = b.CODCEST
 INNER JOIN db_trib_rev.tb_grade_fonseca_sp c ON a.nrotributacao = c.nrotributacao
 WHERE c.tiptributacao = 'EI' -- Entrada Indústria
-AND c.PERTRIBUTADO IN (58.33, 58.34, 38.89)
+AND c.PERTRIBUTADO IN (58.33, 58.34, 38.89) 
+/*Não adicionado filtro para o ccampo PERALIQICMSCALCPRECO = a 0, dessa forma os analistas podem analisar se não existem também tributações ausentes om o Calc 0*/
 GROUP BY 
+    a.NROTRIBUTACAO,
     c.UFCLIENTEFORNEC,
     a.TRIBUTACAO, 
     a.DESCAPLICACAO,
@@ -51,5 +58,13 @@ GROUP BY
     c.PERISENTOCALC, 
     c.PEROUTROCALC, 
     c.PERALIQICMSCALCPRECO, 
-    c.TIPOCALCICMSFISCI
-ORDER BY 8, 1;
+    c.TIPOCALCICMSFISCI,
+    c.SITUACAONF,
+    c.SITUACAONFCALC,
+    c.PERICMSRESOLUCAO13,	
+    c.PERTRIBUTADORESOL13,
+    c.PERISENTORESOL13,
+    c.PEROUTRORESOL13,
+    c.PERACRESCSTRESOLUCAO13,
+    c.PERALIQICMSCALCRESOL13    -- Calc
+ORDER BY 1, 2;
